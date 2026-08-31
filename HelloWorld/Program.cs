648c5f1554ccd3b3e3ab9fc5f0cfce94836d5788
Christoffer Nilsson <ctsa.nilsson@gmail.com>
2026-08-31 10:16:10 +0200

    Created Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console is an object
+// that represents the terminal
+// and has a method named WriteLine
+// that we can call
+// with a string as argument
+// then string will then be written
+// to the terminal
+Console.WriteLine("Hello, World!");
+Console.WriteLine("ERROR!");
